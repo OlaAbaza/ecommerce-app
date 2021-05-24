bf3468a1876d8a4e205032e10c7ea83c51dcce53
Esraa Fathy <fesraa240@gmail.com>
2021-05-24 20:34:49 +0200

    me design

---
+++ app/src/main/java/com/example/shopy/ui/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.shopy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.*
+import com.example.shopy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.me_menu, menu)
+        return  true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.setting -> {
+                true
+            }
+            R.id.shopping_bag -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
