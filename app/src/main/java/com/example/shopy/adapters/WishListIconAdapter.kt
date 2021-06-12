e6acbab64a4fb37f9d40e43fcee9e87acc7ef7b7
fayza55elshorbagy <fayza55elshorbagy@gmail.com>
2021-06-12 03:59:57 +0200

    last payment finish

--- app/src/main/java/com/example/shopy/adapters/WishListIconAdapter.kt
+++ app/src/main/java/com/example/shopy/adapters/WishListIconAdapter.kt
@@ -9,8 +9,10 @@ class WishListIconAdapter(view: View) {
     var wishListCounter : TextView = view.findViewById(R.id.favouriteItems)
     var favouriteButton : ImageButton = view.findViewById(R.id.favouriteButton)
     fun updateView(number : Int){
-        if (number> 0)
-        wishListCounter.text = number.toString()
+        if (number> 0){
+            wishListCounter.text = number.toString()
+            wishListCounter.visibility = View.VISIBLE
+        }
         else
             wishListCounter.visibility = View.INVISIBLE
     }
