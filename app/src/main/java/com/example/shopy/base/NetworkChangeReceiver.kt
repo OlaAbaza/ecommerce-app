9340abac4a510c0e3f3ae3a7dc1dd20ceaf5d97f
Esraa Fathy <fesraa240@gmail.com>
2021-06-13 04:07:25 +0200

    NetworkChangeReceiver

--- app/src/main/java/com/example/shopy/base/NetworkChangeReceiver.kt
+++ app/src/main/java/com/example/shopy/base/NetworkChangeReceiver.kt
@@ -4,17 +4,16 @@ import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+
 import com.example.shopy.base.NetworkUtil.getConnectivityStatusString
-import com.example.shopy.ui.mainActivity.MainActivity
 
 
 class NetworkChangeReceiver(context: Context?) : BroadcastReceiver() {
-    var netwokState: Boolean
+    var networkState: Boolean
     init {
         val status = getConnectivityStatusString(context!!)
-        netwokState = status != NetworkUtil.NETWORK_STATUS_NOT_CONNECTED
-        isOnline = netwokState
+        networkState = status != NetworkUtil.NETWORK_STATUS_NOT_CONNECTED
+        isOnline = networkState
     }
 
     companion object {
@@ -24,11 +23,8 @@ class NetworkChangeReceiver(context: Context?) : BroadcastReceiver() {
     @SuppressLint("LogNotTimber")
     override fun onReceive(context: Context?, intent: Intent) {
         val status = getConnectivityStatusString(context!!)
-        Log.d("TAG", " network reciever")
         if ("android.net.conn.CONNECTIVITY_CHANGE" == intent.action) {
             isOnline = status != NetworkUtil.NETWORK_STATUS_NOT_CONNECTED
         }
-        Log.d("TAG", "status NetworkChangeReceiver ${isOnline}")
-
     }
 }
